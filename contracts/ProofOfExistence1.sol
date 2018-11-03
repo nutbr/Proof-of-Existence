eedebae035b7358fc50787a464e82a0c78afd7eb
nut_br <rui.toledo@uol.com.br>
2018-11-03 12:47:40 -0400

    sucesso2

---
+++ contracts/ProofOfExistence1.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.22;
+
+contract ProofOfExistence1 {
+      // state
+      bytes32 public proof;
+
+      // calculate and store the proof for a document
+      // *transactional function*
+      function notarize(string document) {
+        proof = proofFor(document);
+      }
+
+      // helper function to get a document's sha256
+      // *read-only function*
+      function proofFor(string document) view returns (bytes32) {
+        return sha256(abi.encodePacked(document));
+      }
+
+}
